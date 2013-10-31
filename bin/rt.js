bf0a625f9f1f62843f250d14abb131618634c351
Lars-Erik Roald <lroal@hotmail.com>
2013-10-31 10:29:49 +0100

    can accept folder target from commandline

--- bin/rt.js
+++ bin/rt.js
@@ -1,11 +1,15 @@
 #!/usr/bin/env node
 
-var cwd = process.cwd();
+var dir;
+if (process.argv.length > 2)
+	dir = process.argv[2];
+else
+	dir = process.cwd();
 var runTests = require('./runTests');
 var getExitCode = require('./getExitCode');
 
 process.summary = require('../emptySummary');
-runTests(cwd);
+runTests(dir);
 process.summary();
 var exitCode = getExitCode(process.summary);
 process.exit(exitCode);
