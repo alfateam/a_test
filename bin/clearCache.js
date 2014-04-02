e8b45986752a8f478f35bbac843133aeb3ead5a0
Lars-Erik Roald <lars.roald@gmail.com>
2014-04-02 11:16:26 +0200

    clearCache use expectRequire.clear

--- bin/clearCache.js
+++ bin/clearCache.js
@@ -1,7 +1,11 @@
+var clear = require('a_mock').expectRequire.clear;
+
 function clearCache() {
 	for(var prop in require.cache) {
 		delete require.cache[prop];
 	}
+	if (clear)
+		clear();
 }
 
 module.exports = clearCache;
