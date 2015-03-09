c075c2627cde1ba9a63cbe23ac4675f77636a8ed
Lars-Erik Roald <lars.roald@gmail.com>
2015-03-09 11:47:10 +0100

    ver 0.1.9, inconclusive tests reported as failed in exit code

--- emptySummary.js
+++ emptySummary.js
@@ -1,12 +1,16 @@
 var summary = require('./summary');
-var suites = 0,
-passed = 0,
-failed = 0,
-inconclusive = 0,
-failures = [];
+
+var empty = {
+    suites: 0,
+    passed: 0,
+    failed: 0,
+    inconclusive: 0,
+    failures: []
+};
+
 
 function emptySummary() {
-	return summary(suites, passed, failed, failures, inconclusive);
+    return summary(empty);
 }
 
 module.exports = emptySummary;
