e409af05b7614d9baaae7db45d2588a8e69577eb
Adlan Elm <adlan.elm@gmail.com>
2015-10-22 12:07:28 +0200

    do not clear cached native modules

--- bin/clearCache.js
+++ bin/clearCache.js
@@ -2,7 +2,8 @@ var reset = require('a_mock').expectRequire.reset;
 
 function clearCache() {
 	for(var prop in require.cache) {
-		delete require.cache[prop];
+		if(!prop.match(/\.node$/))
+			delete require.cache[prop];
 	}
 	if (reset)
 		reset();
