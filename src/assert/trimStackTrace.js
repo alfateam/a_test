e444b28c06024cc14086447993c3e27e26b7aa00
lroal <lars.roald@gmail.com>
2017-08-17 10:38:30 +0200

    assertDoesNotThrow works in node 8

--- src/assert/trimStackTrace.js
+++ src/assert/trimStackTrace.js
@@ -1,7 +1,9 @@
 module.exports = function(e) {
 	var stack = e.stack;
-	var index = stack.indexOf('at _throws (assert.js:');
-
+	var index = stack.indexOf('at innerThrows (assert.js:');
+	if (index < 0)
+		index = stack.indexOf('at _throws (assert.js:');
+	
 	while(stack[index] != '\n'){
 		index --;
 	}
